2ecf4a28602349eb51b7be63ec861f795765b0aa
Lewis Sideras <lewis.sideras@gmail.com>
2026-02-04 20:20:55 -0500

    fixing spelling error

--- cc_3a.js
+++ cc_3a.js
@@ -35,9 +35,10 @@ customers[0].purchases.push("force push");
 
 // Display Customer Information
 
-customers.forEach(customer => {
-    console.log(`Name: ${customer.name}`);
-    console.log(`Email: ${customer.email}`);
-    console.log(`Purchases: ${customer.purchases.join(", ")}`);
+customers.forEach(customers => {
+    console.log(`Name: ${customers.name}`);
+    console.log(`Email: ${customers.email}`);
+    console.log(`Purchases: ${customers.purchases.join(", ")}`);
     console.log('---');
 });
+
